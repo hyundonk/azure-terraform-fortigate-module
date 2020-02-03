dc9a9ff7728906fa87b6eb0b0d3cf5d0d764f6cc
Calvin Kim <hyuk@microsoft.com>
2020-02-03 21:08:53 +0900

    changed fortigate image version to 6.2.3

--- quick-test/fortigate/fortigate.tf
+++ quick-test/fortigate/fortigate.tf
@@ -85,8 +85,8 @@ resource "azurerm_virtual_machine" "fw" {
     publisher = "fortinet" 
     offer     = "fortinet_fortigate-vm_v5"
     sku       = "fortinet_fg-vm"
-    #version   = "6.2.3"
-    version   = "6.0.7"
+    version   = "6.2.3"
+    #version   = "6.0.7"
   }
 
   # plan information required for marketplace images
